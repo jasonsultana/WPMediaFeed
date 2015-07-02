03a7c6ab80a1affef02db55dc926c52f50bdd9fd
Jason Sultana <kowboy_koder@hotmail.com>
2015-07-02 22:39:30 +0900

    Alpha release
    Done some preliminary tests. The plugin now works with a shortcode
    [WPYoutubeFeed].

--- WPYoutubeFeed.php
+++ WPYoutubeFeed.php
@@ -2,13 +2,15 @@
 /*
 Plugin Name: WPYoutubeFeed
 Plugin URI:  
-Description: A Youtube Feed plugin for Wordpress.
+Description: A Youtube Feed plugin for Wordpress. Use [WPYoutubeFeed] on any page or post where you want to load your Youtube feed.
 Version:     0.1-alpha
 Author:      Jason Sultana
 Author URI:  https://wordpress.org/support/profile/kowboykoder
 Text Domain: 
 Domain Path: /
  */
+	defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+	
 	function autoload() {
 		spl_autoload_register(function($class) {
 			$dir = dirname(__FILE__) . '/core/' . $class . '.class.php';
@@ -26,6 +28,46 @@ Domain Path: /
 	admin_render_settings();
 
 	function WPYoutubeFeed_content_filter($content) {
+		$url = get_option('WPYoutubeFeed_channel');
+
+		if(!$url || $url == "")
+			return $content;
+
+		//Only get the feed if the shortcode is present.
+		if(strpos($content, "[WPYoutubeFeed]") === false)
+			return $content;
+
+		$content = str_replace("[WPYoutubeFeed]", "", $content);
+
+		//First, download the channel HTML from youtube
+		require_once dirname(__FILE__) . '/core/HTTPRequest.class.php';
+		$req = new HTTPRequest($url);
+		$html = $req->DownloadToString();
+
+		//Next, parse it to get the video IDs
+		$videoParts = explode("watch?v=", $html);
+		$videoIds = array();
+
+		//skip the first one - it's bogus
+		for($i = 1; $i < sizeof($videoParts); $i++) {
+			$part = $videoParts[$i];
+			$videoId = strstr($part, "\"", true);
+
+			//echo "VideoId: $videoId , length: " . strlen($videoId) . "<br>";
+
+			//Youtube videoIds should always be 11 chars long, but let's keep it open for future compatibility
+			if( strlen($videoId) > 10 ) {
+				if(!in_array($videoId, $videoIds)) {
+					array_push($videoIds, $videoId);
+				}
+			}
+		}
+
+		//Finally, output the embed links to the unique videos
+		foreach($videoIds as $videoId) {
+			$content .= '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+		}
+
 		return $content;
 	}
 	add_filter( 'the_content', 'WPYoutubeFeed_content_filter');
